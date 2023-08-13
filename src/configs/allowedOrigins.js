4ed494aa8d246da2d13be8b417e75174920b795d
cosmas <cosmasthuku4@gmail.com>
2023-08-13 12:08:44 +0300

    add an allowed origin

--- src/configs/allowedOrigins.js
+++ src/configs/allowedOrigins.js
@@ -8,7 +8,9 @@ const allowedOrigins = [
         'http://localhost:5173',
         'https://localhost:5173',
         'http://localhost:5173/',
-        'https://localhost:5173/'
+        'https://localhost:5173/',
+        'https://occupational-safety.netlify.app/',
+        'https://occupational-safety.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
