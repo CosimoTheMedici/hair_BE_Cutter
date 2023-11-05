c11eb0db589790eb3f8c4f4802dffbb186f30de1
cosmas <cosmasthuku4@gmail.com>
2023-11-05 16:22:47 +0300

    fix:token

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const cors = require('cors');
-const bodyParser = require('body-parser')
-const { logger } = require('./src/middleware/logEvents');
-const { corsOptions } = require('./src/configs/corsOptions');
-const { errorHandle } = require('./src/middleware/errorHandler');
-const cookieParser = require('cookie-parser');
-const credentials = require('./src/middleware/credentials');
+const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { logger } = require("./src/middleware/logEvents");
+const { corsOptions } = require("./src/configs/corsOptions");
+const { errorHandle } = require("./src/middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const credentials = require("./src/middleware/credentials");
 const PORT = process.env.PORT || 3500;
-require('dotenv').config();
-
-
+require("dotenv").config();
 
 //custom middleware logger
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger);
-
 
 app.use(credentials);
 
@@ -27,23 +24,24 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.get('/',(req,res)=> res.json("SERVER Working..p  "+process.env.REFRESH_TOKEN_SECRET));
-app.use('/create',require('./src/routes/Router'));
-app.use('/api/v1/auth/',require('./src/routes/authRouter'));
-app.use('/api/v1/properties',require('./src/routes/propertiesRouter'));
-app.use('/api/v1/units',require('./src/routes/unitsRouter'));
-app.use('/api/v1/utilities',require('./src/routes/utilityRouter'));
-app.use('/api/v1/tenants',require('./src/routes/tenantRouter'));
-app.use('/api/v1/payments',require('./src/routes/paymentRouter'));
-app.use('/api/v1/agents',require('./src/routes/agentRouter'));
-app.use('/api/v1/transactions',require('./src/routes/transactionRouter'));
-app.use('/api/v1/watchmania',require('./src/routes/watchRouter'));
+app.get("/", (req, res) =>
+  res.json("SERVER Working..p  " + process.env.PRODUCT_COOKIE_TOKEN_SECRET)
+);
+app.use("/create", require("./src/routes/Router"));
+app.use("/api/v1/auth/", require("./src/routes/authRouter"));
+app.use("/api/v1/properties", require("./src/routes/propertiesRouter"));
+app.use("/api/v1/units", require("./src/routes/unitsRouter"));
+app.use("/api/v1/utilities", require("./src/routes/utilityRouter"));
+app.use("/api/v1/tenants", require("./src/routes/tenantRouter"));
+app.use("/api/v1/payments", require("./src/routes/paymentRouter"));
+app.use("/api/v1/agents", require("./src/routes/agentRouter"));
+app.use("/api/v1/transactions", require("./src/routes/transactionRouter"));
+app.use("/api/v1/watchmania", require("./src/routes/watchRouter"));
 //app.use('/api/busness/',require('./src/routesbusinessRouter'));
 
-app.get('/', (req, res) => {
-    res.send('Hello worlldk')
-
+app.get("/", (req, res) => {
+  res.send("Hello worlldk");
 });
 
-app.use(errorHandle)
-app.listen(PORT, ()=> console.log(`server running on ${PORT}`))
+app.use(errorHandle);
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
